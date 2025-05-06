bfa144c45ac96bdefa578c63a799f1136b46802c
Syahrullah <114345060+SyahrullahIlyas@users.noreply.github.com>
2025-05-06 19:55:13 +0800

    nonaftifkan wa

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const munculkan = document.querySelector(".wa");
-window.addEventListener("scroll", function () {
-  munculkan.classList.add("munculkan");
-});
+// const munculkan = document.querySelector(".wa");
+// window.addEventListener("scroll", function () {
+//   munculkan.classList.add("munculkan");
+// });
